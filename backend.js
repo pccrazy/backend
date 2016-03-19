8525fcfa5570e3720ab66b3dd3458ab2907fdca3
pccrazy <anwarsalim07@gmail.com>
2016-03-19 18:31:03 +0400

    still struggling with the create job

--- backend.js
+++ backend.js
@@ -69,20 +69,26 @@ var Pushbots = new pushbots.api({
        if(!err) {
          console.log(rows.length);
          if(rows.length==0){
-           if(tools.sendJobtoRas(req.body.job,req.body.deviceStatus)=="ack"){
-             pool.query(devicesmode,function(err,res){
-             if(err) {
-                   respond.send("Somthing Went Wrong");
-                    console.log("Error"+err);
-                   }else{
+           request.post({url:'http://10.8.0.6:4000/job', form: {job:job,dstatus:dstatus}},
+           function(err,httpResponse,body)
+             {
+               console.log(httpResponse);
+               if(!err){
+               pool.query(devicesmode,function(err,res){
+               if(err) {
+                     respond.send("Somthing Went Wrong");
+                      console.log("Error"+err);
+                     }else{
 
-                        respond.send("Job is successfully created");
-                        console.log('Last insert ID:', res.insertId);
-                    }
+                          respond.send("Job is successfully created");
+                          console.log('Last insert ID:', res.insertId);
+                      }
+               });
+             }else{
+                respond.send("Somthing Wrong With The Connection");
+             }
+               return httpResponse;
              });
-           }else{
-               respond.send("Connection error with system");
-           }
          }else{
                respond.send("You already have a job at that Time and Date");
          }
