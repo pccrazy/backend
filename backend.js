8c3d8aab7dd62da487b9cd58fc5f2ecf69358fe9
anwar <anwarsalim07@gmail.com>
2016-03-26 23:11:50 +0400

    token

--- backend.js
+++ backend.js
@@ -128,9 +128,15 @@ var tools=require("./Tools")
  });
 
  app.post("/token",function(req,respond){
-   console.log(req.body.token+" "+req.body.user);
-  var devicesmode="UPDATE `SmartHouse`.`Users` SET GCM='"+req.body.token+"' WHERE idUsers="+req.body.user;
-  handle_database(req,respond,devicesmode);
+  console.log(req.body.token+" "+req.body.user);
+  var token="UPDATE `SmartHouse`.`Users` SET GCM='"+req.body.token+"' WHERE idUsers="+req.body.user;
+  pool.query(token, function(err, results) {
+    if (err) {console.log("somthing went wrong")};
+    // `results` is an array with one element for every statement in the query:
+    respond.json("ack token");
+    console.log(results); // [{1: 1}]
+  // [{2: 2}]
+  });
  });
 
 
